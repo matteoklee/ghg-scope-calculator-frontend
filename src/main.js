37f1cc26f4aded964602da5c52aadef3d4436a17
Matteo Kleemann <117347339+matteoklee@users.noreply.github.com>
2025-03-24 20:53:00 +0100

    try fixing styling bug due to css styles coming along with vuetify installation

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-import './assets/main.css';
-
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
 import App from './App.vue';
 import router from './router';
 import vuetify from '@/plugins/vuetify.js'
+
+import './assets/main.css';
+import './assets/vuetify-overrides.css';
 
 const app = createApp(App);
 
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,12 +1,29 @@
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import 'vuetify/styles'
+//import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
 
 const vuetify = createVuetify({
   components,
   directives,
+  theme: {
+    defaultTheme: 'customTheme',
+    themes: {
+      customTheme: {
+        dark: false,
+        colors: {
+          primary: '#0A0B0D',
+          /*secondary: '#424242',
+          accent: '#82B1FF',
+          error: '#FF5252',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FFC107',*/
+        },
+      },
+    },
+  },
 })
 
 export default vuetify
