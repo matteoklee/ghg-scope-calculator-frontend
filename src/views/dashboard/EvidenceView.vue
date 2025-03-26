cc5f231ffa7dfa86ba15472746b7c8f4b7d8c9d4
Matteo Kleemann <117347339+matteoklee@users.noreply.github.com>
2025-03-26 01:51:26 +0100

    added some default content for dashboard sub pages

--- src/views/dashboard/EvidenceView.vue
+++ src/views/dashboard/EvidenceView.vue
@@ -1,12 +1,35 @@
 <script>
+import { ArrowLeft } from 'lucide-vue-next';
+import { Button } from '@/components/ui/button/index.js';
+
 export default {
   name: 'EvidenceView',
+  components: { Button, ArrowLeft },
 };
 </script>
 
 <template>
-  <div class="">
-    <h1>This is the evidence page</h1>
+  <div class="m-12">
+    <div class="">
+      <div class="flex justify-between items-center">
+        <div>
+          <h1 class="text-4xl mb-6">Nachweise & Validierung</h1>
+        </div>
+        <div>
+          <router-link to="/dashboard">
+            <Button variant="">
+              <ArrowLeft class="w-4 h-4"></ArrowLeft>
+              <span>Zurück</span>
+            </Button>
+          </router-link>
+        </div>
+      </div>
+      <p class="text-lg text-gray-700 text-justify mb-4">
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+      </p>
+    </div>
+    <div class="mt-8"></div>
   </div>
 </template>
 
